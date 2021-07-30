a17ea433aea3d23435e17c659e2df5ed0c666151
Evgeniy Kukushkin <eugin86@list.ru>
2021-07-30 23:02:54 +0500

    TO DO 14/06/2021

--- src/main/java/lesson01/ClientHandler.java
+++ src/main/java/lesson01/ClientHandler.java
@@ -1,9 +1,6 @@
 package lesson01;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
@@ -21,30 +18,33 @@ public class ClientHandler implements Runnable {
             while (true) {
                 String command = in.readUTF();
                 if ("upload".equals(command)) {
-                    try {
-                        File file = new File("server"  + File.separator + in.readUTF());
-                        if (!file.exists()) {
-                            file.createNewFile();
-                        }
-                        FileOutputStream fos = new FileOutputStream(file);
-
-                        long size = in.readLong();
-
-                        byte[] buffer = new byte[8 * 1024];
-
-                        for (int i = 0; i < (size + (buffer.length - 1)) / (buffer.length); i++) {
-                            int read = in.read(buffer);
-                            fos.write(buffer, 0, read);
-                        }
-                        fos.close();
-                        out.writeUTF("OK");
-                    } catch (Exception e) {
-                        out.writeUTF("FATAL ERROR");
-                    }
+//                    try {
+//                        File file = new File("server"  + File.separator + in.readUTF());
+//                        if (!file.exists()) {
+//                            file.createNewFile();
+//                        }
+//                        FileOutputStream fos = new FileOutputStream(file);
+//
+//                        long size = in.readLong();
+//
+//                        byte[] buffer = new byte[8 * 1024];
+//
+//                        for (int i = 0; i < (size + (buffer.length - 1)) / (buffer.length); i++) {
+//                            int read = in.read(buffer);
+//                            fos.write(buffer, 0, read);
+//                        }
+//                        fos.close();
+//                        out.writeUTF("OK");
+//                    } catch (Exception e) {
+//                        out.writeUTF("FATAL ERROR");
+//                    }
+                    uploading(out, in);
                 }
 
                 if ("download".equals(command)) {
                     // TODO: 14.06.2021
+                    downloading(out, in);
+
                 }
                 if ("exit".equals(command)) {
                     System.out.printf("Client %s disconnected correctly\n", socket.getInetAddress());
@@ -56,6 +56,58 @@ public class ClientHandler implements Runnable {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void downloading(DataOutputStream out, DataInputStream in) throws IOException {
+        String filename = in.readUTF();
+        try {
+            File file = new File("server" + File.separator + filename);
+            if (!file.exists()) {
+                throw  new FileNotFoundException();
+            }
+
+            long fileLength = file.length();
+
+            FileInputStream fis = new FileInputStream(file);
+
+            out.writeLong(fileLength);
+
+            int read;
+            byte[] buffer = new byte[8 * 1024];
+            while ((read = fis.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+
+            out.flush();
+
+            String status = in.readUTF();
+            System.out.println("downloading status: " + status);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found - " + filename);
+        }
+    }
+
+    private void uploading(DataOutputStream out, DataInputStream in) throws IOException {
+        try {
+            File file = new File("server"  + File.separator + in.readUTF());
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(file);
+
+            long size = in.readLong();
+
+            byte[] buffer = new byte[8 * 1024];
+
+            for (int i = 0; i < (size + (buffer.length - 1)) / (buffer.length); i++) {
+                int read = in.read(buffer);
+                fos.write(buffer, 0, read);
+            }
+            fos.close();
+            out.writeUTF("OK");
+        } catch (Exception e) {
+            out.writeUTF("FATAL ERROR");
         }
     }
 }
